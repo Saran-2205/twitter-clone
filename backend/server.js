45c0b1ff0dab2a836f02f977580e0b27cf04db25
Saran Jayan <srinivasansaranjayan@gmail.com>
2025-02-17 00:04:53 +0530

    1 - auth setup

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,16 @@ import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
 
 
 const app = express();
 const PORT=process.env.PORT||5000
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use(cookieParser());
 
 dotenv.config();
 
