e355df66f16572192cd7bc52298192968ba8699d
Saran Jayan <srinivasansaranjayan@gmail.com>
2025-03-02 18:47:41 +0530

    1-Post route and controllers added

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,12 @@ import {v2 as cloudinary} from "cloudinary";
 
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/users.routes.js";
+import postRoutes from "./routes/post.routes.js";
 
 import connectMongoDB from "./db/connectMongoDB.js";
+
+
+dotenv.config();
 
 
 const app = express();
@@ -18,15 +22,15 @@ app.use(express.urlencoded({extended:true}));
 
 app.use(cookieParser());
 
-dotenv.config();
 cloudinary.config({
-    cloud_name:CLOUDINARY_CLOUD_NAME,
-    api_key:CLOUDINARY_API_KEY,
-    api_secret:CLOUDINARY_API_SECRET
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET
 });
 
 app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
+app.use("/api/posts",postRoutes);
 
 app.listen(PORT,()=>{
     console.log(`server is running on port ${PORT}`);
