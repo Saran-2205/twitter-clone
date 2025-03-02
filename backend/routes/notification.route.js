b1a3e10698210464a4e513a539f787d82af666a2
Saran Jayan <srinivasansaranjayan@gmail.com>
2025-03-02 19:26:38 +0530

    1- Notification routes and controllers added

---
+++ backend/routes/notification.route.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { protectRoute } from "../middleware/protectRoute.js";
+import { deleteAllNotifications, getNotifications } from "../controllers/notification.controller.js";
+
+const router=express.Router();
+
+router.get("/",protectRoute,getNotifications);
+router.delete("/",protectRoute,deleteAllNotifications);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import {v2 as cloudinary} from "cloudinary";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/users.route.js";
 import postRoutes from "./routes/post.route.js";
+import notificationRoutes from "./routes/notification.route.js";
 
 import connectMongoDB from "./db/connectMongoDB.js";
 
@@ -31,6 +32,7 @@ cloudinary.config({
 app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
 app.use("/api/posts",postRoutes);
+app.use("/api/notifications",notificationRoutes);
 
 app.listen(PORT,()=>{
     console.log(`server is running on port ${PORT}`);
