52ce9d00689668740a486df874de9bfc3499514b
Saran Jayan <srinivasansaranjayan@gmail.com>
2025-03-15 19:45:35 +0530

    3- use follow hook added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,9 +5,18 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
+const defaultQueryFn = async ({ queryKey }) => {
+  const { data } = await axios.get(
+    `https://jsonplaceholder.typicode.com${queryKey[0]}`,
+  )
+  return data
+}
+
+
 const queryClient = new QueryClient({
   defaultOptions:{
     queries:{
+      queryFn:defaultQueryFn,
       refetchOnWindowFocus:false,
     }
   }
