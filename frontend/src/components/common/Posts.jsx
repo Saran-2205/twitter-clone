106b65314ee23281acfd694eb7db1c7e11ba277d
Saran Jayan <srinivasansaranjayan@gmail.com>
2025-03-16 18:51:07 +0530

    3- Added profilepage functiionality and user info fetched

--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -1,9 +1,9 @@
 import Post from "./Post";
 import PostSkeleton from "../skeletons/PostSkeleton";
-import {useQuery} from "@tanstack/react-query"
+import { useQuery } from "@tanstack/react-query"
 import { useEffect } from "react";
 
-const Posts = ({ feedType }) => {
+const Posts = ({ feedType, username, userId}) => {
 
 	const getPostEndPoint = () => {
 		switch (feedType) {
@@ -11,22 +11,26 @@ const Posts = ({ feedType }) => {
 				return "/api/posts/all";
 			case "following":
 				return "/api/posts/following";
+			case "posts":
+				return `/api/posts/user/${username}`;
+			case "likes":
+				return `/api/posts/likes/${userId}`;
 			default:
 				return "/api/posts/all";
 		}
 	}
 
-	const POST_ENDPOINT=getPostEndPoint();
+	const POST_ENDPOINT = getPostEndPoint();
 
-	const {data:posts,isLoading,refetch,isRefetching}= useQuery({
-		queryKey:["posts"],
-		queryFn:async()=>{
+	const { data: posts, isLoading, refetch, isRefetching } = useQuery({
+		queryKey: ["posts"],
+		queryFn: async () => {
 			try {
-				const res= await fetch(POST_ENDPOINT);
-				const data= await res.json();
+				const res = await fetch(POST_ENDPOINT);
+				const data = await res.json();
 
-				if(!res.ok){
-					throw new Error(data.error||"Something went wrong");
+				if (!res.ok) {
+					throw new Error(data.error || "Something went wrong");
 				}
 
 				return data
@@ -36,21 +40,21 @@ const Posts = ({ feedType }) => {
 		}
 	})
 
-	useEffect(()=>{
+	useEffect(() => {
 		refetch();
-	},[feedType,refetch])
+	}, [feedType, refetch])
 
 	return (
 		<>
-			{(isLoading||isRefetching) && (
+			{(isLoading || isRefetching) && (
 				<div className='flex flex-col justify-center'>
 					<PostSkeleton />
 					<PostSkeleton />
 					<PostSkeleton />
 				</div>
 			)}
-			{!isLoading && !isRefetching && posts?.length === 0 && <p className='text-center my-4'>No posts in this tab. Switch 👻</p>}
-			{!isLoading && !isRefetching&& posts && (
+			{!isLoading && !isRefetching && posts?.length === 0 && <p className='text-center my-4'>No posts in this tab. </p>}
+			{!isLoading && !isRefetching && posts && (
 				<div>
 					{posts.map((post) => (
 						<Post key={post._id} post={post} />
